a123f8585644ecdc3d123567c2556ae7c5781059
Jadegomez <jade.gomez@studentfr.ch>
2025-02-05 11:23:34 +0100

    final

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,14 @@ var articles = document.querySelectorAll(".articles-item");
 
 function research() {
   var text = input.value;
+  articles.forEach(function (article) {
+    console.log(article.innerText);
+    if (article.textContent.includes(text)) {
+      article.style.display = "block";
+    } else {
+      article.style.display = "none";
+    }
+  });
 }
 
-button.addEventListener("click");
+button.addEventListener("click", research);
